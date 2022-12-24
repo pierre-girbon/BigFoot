a944f67dda1d460c41468ccf1e39cb98c842291e
Pierre <pierre.girbon@gmail.com>
2022-12-24 17:58:38 +0100

    Roalnd MC 707 setup

--- Firmware/src/main.cpp
+++ Firmware/src/main.cpp
@@ -14,24 +14,27 @@ BidirectionalMIDI_PipeFactory<2> pipes;
 CD74HC4051 switchMux {5, {2, 3, 4}};
 
 // Footswitches initialization
-Bank<4> preset(8);
+Bank<2> bank(8);
+IncrementDecrementSelector<2> selector {
+	bank,
+	{7,8},
+	Wrap::Wrap,
+};
 
-IncrementDecrementSelector<4> selector {preset, {7, 8}, Wrap::Wrap};
-
-Bankable::PCButton PCChange1 [] {
-	{ {preset, BankType::CHANGE_ADDRESS}, switchMux.pin(0), {0, CHANNEL_16} },
-	{ {preset, BankType::CHANGE_ADDRESS}, switchMux.pin(1), {1, CHANNEL_16} },
-	{ {preset, BankType::CHANGE_ADDRESS}, switchMux.pin(2), {2, CHANNEL_16} },
-	{ {preset, BankType::CHANGE_ADDRESS}, switchMux.pin(3), {3, CHANNEL_16} },
-	{ {preset, BankType::CHANGE_ADDRESS}, switchMux.pin(4), {4, CHANNEL_16} },
-	{ {preset, BankType::CHANGE_ADDRESS}, switchMux.pin(5), {5, CHANNEL_16} },
-	{ {preset, BankType::CHANGE_ADDRESS}, switchMux.pin(6), {6, CHANNEL_16} },
-	{ {preset, BankType::CHANGE_ADDRESS}, switchMux.pin(7), {7, CHANNEL_16} },
+Bankable::PCButton PCChange [] {
+	{ {bank, BankType::CHANGE_ADDRESS}, switchMux.pin(0), {0, CHANNEL_16} },
+	{ {bank, BankType::CHANGE_ADDRESS}, switchMux.pin(1), {1, CHANNEL_16} },
+	{ {bank, BankType::CHANGE_ADDRESS}, switchMux.pin(2), {2, CHANNEL_16} },
+	{ {bank, BankType::CHANGE_ADDRESS}, switchMux.pin(3), {3, CHANNEL_16} },
+	{ {bank, BankType::CHANGE_ADDRESS}, switchMux.pin(4), {4, CHANNEL_16} },
+	{ {bank, BankType::CHANGE_ADDRESS}, switchMux.pin(5), {5, CHANNEL_16} },
+	{ {bank, BankType::CHANGE_ADDRESS}, switchMux.pin(6), {6, CHANNEL_16} },
+	{ {bank, BankType::CHANGE_ADDRESS}, switchMux.pin(7), {7, CHANNEL_16} },
 };
 
 //Expression Pedals init
 CCPotentiometer expr1 {
-	A0, {MIDI_CC::Channel_Volume, CHANNEL_16}
+	A0, {MIDI_CC::Modulation_Wheel, CHANNEL_1}
 };
 
 void setup() {
